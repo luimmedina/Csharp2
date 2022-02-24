5487658945c253a7fb10f77c3f0650690c6da51f
Luis Medina <78856862+luimmedina@users.noreply.github.com>
2022-02-24 17:20:59 -0500

    Add files via upload

---
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using CSharpFundamentals.Math;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person luis = new Person();
+            luis.FirstName = "Luis";
+            luis.LastName = "Medina";
+            luis.Introduce();
+
+            Age age = new Age();
+            var ageperson = age.AgePerson(17);
+
+            Console.WriteLine("My Age is " + ageperson);
+        }
+    }
+}
---
+++ CSharpFundamentals3/CSharpFundamentals3/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+namespace CSharpFundamentals3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Luis";
+            var lastName = "Medina";
+
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+
+            var names = new string[3] { "John", "Jack", "Mary" };
+
+            var formattedNames = string.Join(",", names);
+
+            Console.WriteLine(formattedNames);
+
+            var text="Hi John\nLook into the following paths\nc:\\folder1\\folder2\nc:\\folder3\\folder4";
+            Console.WriteLine(text);
+
+            /*
+             Another form:
+           @"Hi John
+            Look into the following paths
+            c:\folder1\folder2
+            c:\folder3\folder4";
+            */
+
+        }
+    }
+}
